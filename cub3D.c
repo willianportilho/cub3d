4d0da9db6ecfd6a300be97f6bcfd7fe36d62f5cd
rgenge <rgenge@terra.com.br>
2022-12-20 19:53:55 -0400

    check_args finalizado

--- cub3D.c
+++ cub3D.c
@@ -6,28 +6,32 @@
 /*   By: acosta-a <acosta-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 15:41:17 by acosta-a          #+#    #+#             */
-/*   Updated: 2022/12/20 17:04:32 by acosta-a         ###   ########.fr       */
+/*   Updated: 2022/12/20 19:51:53 by acosta-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
-//função abaixo não esta pronta mas será pra checar se o argumento termina com .cub
+//checagem de argumentos finalizada, estou checando se o arquivo mapa é .cub, caso não seja programa é finalizado
 
-int	check_args (char **argv)
+void	check_args (char **argv)
 {
 	int		map_len;
 	char	*map_name;
 
 	map_len = ft_strlen (argv[1]);
-	if (map_len > 5)
-		map_name = ft_substr(argv[1], (map_len - 5), 5);
-	if (ft_strncmp(map_name, ".cub", 5))
-	{
-		printf("testando");
-		return (0);
-	}
+	if (map_len > 4)
+		map_name = ft_substr(argv[1], (map_len - 4), 5);
 	else
-		exit(1);
+		print_exit("Map must be .cub\n");
+	printf("%d \n", map_len);
+	if (!ft_strncmp(map_name, ".cub", 5))
+	{
+		printf("%s \n", map_name);
+		free(map_name);
+		return ;
+	}
+	free(map_name);
+	print_exit("Map must be .cub\n");
 }
 
 //main principal já checa quantos argumentos e se for != de 2 ele sai
@@ -36,4 +40,6 @@ int main (int argc, char **argv)
 	if (argc != 2)
 		print_exit("Error\nType 2 arguments");
 	check_args(argv);
+	// vou iniciar agora o parser dos argumentos iniciais antes do mapa
+//	parser_map_args();
 }
--- cub3D.h
+++ cub3D.h
@@ -6,12 +6,12 @@
 /*   By: acosta-a <acosta-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 15:41:53 by acosta-a          #+#    #+#             */
-/*   Updated: 2022/12/20 16:50:00 by acosta-a         ###   ########.fr       */
+/*   Updated: 2022/12/20 19:50:39 by acosta-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
 # include <stdlib.h>
 # include <unistd.h>
@@ -21,9 +21,7 @@
 //acho bom colocar no header cada função separada pelo nome do arquivo
 /* cub3D.c*/
 
-
 /* exit_utils.c*/
 void	print_exit(char *input);
 
-
-#endif
+# endif
