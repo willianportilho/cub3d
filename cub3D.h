6d87aae7b0cec0e3d357dea798bdba0ded4fc099
wportilh <willianportilho01@gmail.com>
2023-01-20 22:21:02 -0300

    optimization castle.cub (xpms resolution); changed TILE_SIZE value from 64 to 512

--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 15:41:53 by acosta-a          #+#    #+#             */
-/*   Updated: 2023/01/20 19:53:33 by wportilh         ###   ########.fr       */
+/*   Updated: 2023/01/20 22:18:44 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 # define PI 3.14159265359
 # define WHITE 0x0000FF00 // só pra testar
 # define BLUE 0x000000FF // só pra testar
-# define TILE_SIZE 64 // tamanho dos .xpm
+# define TILE_SIZE 512 // tamanho dos .xpm
 # define UP 1
 # define DOWN 0
 # define RIGHT 2
